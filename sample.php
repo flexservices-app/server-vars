332e5b89c4c9cfda3b3b915b3688cf2fa0226cd2
Flex Services (Security - Firewall) <info@flexservices.app>
2026-02-10 21:04:15 +0100

    PHP - $_SERVER

---
+++ sample.php
@@ -0,0 +1,9 @@
+PHP - $_SERVER
+-------------------------
+
+
+echo "<br>Host : " . $_SERVER["HTTP_X_FIREWALL_HOST"];
+echo "<br>IP : " .$_SERVER["HTTP_X_FIREWALL_IP"];
+echo "<br>Country : " .$_SERVER["HTTP_X_FIREWALL_COUNTRY"];
+echo "<br>ASN : " .$_SERVER["HTTP_X_FIREWALL_ASN"];
+echo "<br>ISP : " .$_SERVER["HTTP_X_FIREWALL_ISP"];
